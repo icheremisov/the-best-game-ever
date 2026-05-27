e5f8004decd0f7ad7ce11c1ec9bbe1600e14d1ea
Ivan Cheremisov <devalidator@gmail.com>
2026-05-27 21:17:58 +0400

    feat(ui): popup controllers (intro/surrender/end-of-day)

---
+++ Game/Assets/Code/UI/AdventurerIntroPopup.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Mimic.Data;
+
+namespace Mimic.UI
+{
+    public class AdventurerIntroPopup : MonoBehaviour
+    {
+        public Text NameText;
+        public Text PhraseText;
+        public Button EatButton;
+
+        private Action onEat;
+
+        private void Awake()
+        {
+            if (EatButton != null)
+                EatButton.onClick.AddListener(() => { onEat?.Invoke(); gameObject.SetActive(false); });
+            gameObject.SetActive(false);
+        }
+
+        public void Show(AdventurerData data, Action onEatCallback)
+        {
+            if (NameText != null) NameText.text = data.Name;
+            if (PhraseText != null) PhraseText.text = data.Phrase;
+            onEat = onEatCallback;
+            gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Game/Assets/Code/UI/EndOfDayPopup.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Mimic.UI
+{
+    public class EndOfDayPopup : MonoBehaviour
+    {
+        public Text TitleText;
+        public Text SubtitleText;
+        public Button RetryButton;
+        public Button MenuButton;
+
+        private void Awake()
+        {
+            if (RetryButton != null)
+                RetryButton.onClick.AddListener(() => SceneManager.LoadScene("Game"));
+            if (MenuButton != null)
+                MenuButton.onClick.AddListener(() => SceneManager.LoadScene("MainMenu"));
+            gameObject.SetActive(false);
+        }
+
+        public void ShowWin(int gold, int quota)
+        {
+            if (TitleText != null) TitleText.text = "День спасён!";
+            if (SubtitleText != null) SubtitleText.text = $"Заработано {gold} / {quota}";
+            if (RetryButton != null) RetryButton.gameObject.SetActive(false);
+            gameObject.SetActive(true);
+        }
+
+        public void ShowLose(string reason, int gold, int quota)
+        {
+            if (TitleText != null) TitleText.text = reason;
+            if (SubtitleText != null) SubtitleText.text = $"Заработано {gold} / {quota}";
+            if (RetryButton != null) RetryButton.gameObject.SetActive(true);
+            gameObject.SetActive(true);
+        }
+    }
+}
