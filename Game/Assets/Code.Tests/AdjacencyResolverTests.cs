533e2abcf5be9878b6678c97c4a8a483d61e3877
Ivan Cheremisov <devalidator@gmail.com>
2026-05-31 15:34:26 +0400

    test(adjacency): постоянные тесты резолвера на вайлдкард/стак/аддитив/мульти-таргет

--- Game/Assets/Code.Tests/AdjacencyResolverTests.cs
+++ Game/Assets/Code.Tests/AdjacencyResolverTests.cs
@@ -114,5 +114,91 @@ namespace Mimic.Tests
                                                    i => i.AdjacencyRules);
             Assert.AreEqual(10 + 12, result.TotalGold);
         }
+
+        // --- стак / различные инстансы / аддитив / мульти-таргет / вайлдкард ---
+
+        [Test]
+        public void Resolve_Stackable_CountsEachInstance()
+        {
+            var grid = new GridModel<Item>(3, 3);
+            var diamond = Mk("diamond", "X", 100, 1, adjTarget: "hat", adjEffect: "gold:+50%*");
+            grid.TryPlace(diamond, 1, 1, Rotation.Deg0);
+            grid.TryPlace(Mk("hat", "X", 0, 1), 0, 1, Rotation.Deg0);
+            grid.TryPlace(Mk("hat", "X", 0, 1), 2, 1, Rotation.Deg0);
+            var result = AdjacencyResolver.Resolve(grid, i => i.Id, i => i.Gold, i => i.AcidCost,
+                                                   i => i.AdjacencyRules);
+            Assert.AreEqual(200, result.GetGold(diamond)); // +50%*2 = +100%
+        }
+
+        [Test]
+        public void Resolve_NonStackable_AppliesOnce_RegardlessOfCount()
+        {
+            var grid = new GridModel<Item>(3, 3);
+            var diamond = Mk("diamond", "X", 100, 1, adjTarget: "hat", adjEffect: "gold:+50%");
+            grid.TryPlace(diamond, 1, 1, Rotation.Deg0);
+            grid.TryPlace(Mk("hat", "X", 0, 1), 0, 1, Rotation.Deg0);
+            grid.TryPlace(Mk("hat", "X", 0, 1), 2, 1, Rotation.Deg0);
+            var result = AdjacencyResolver.Resolve(grid, i => i.Id, i => i.Gold, i => i.AcidCost,
+                                                   i => i.AdjacencyRules);
+            Assert.AreEqual(150, result.GetGold(diamond)); // +50% один раз
+        }
+
+        [Test]
+        public void Resolve_Stackable_DoubleTouchOfSameInstance_CountsAsOne()
+        {
+            // source 'XX' и target 'XX' соприкасаются двумя рёбрами, но это ОДИН инстанс соседа.
+            var grid = new GridModel<Item>(3, 3);
+            var diamond = Mk("diamond", "XX", 100, 1, adjTarget: "hat", adjEffect: "gold:+50%*");
+            grid.TryPlace(diamond, 0, 0, Rotation.Deg0); // занимает (0,0)-(1,0)
+            grid.TryPlace(Mk("hat", "XX", 0, 1), 0, 1, Rotation.Deg0); // (0,1)-(1,1): касается обеих клеток
+            var result = AdjacencyResolver.Resolve(grid, i => i.Id, i => i.Gold, i => i.AcidCost,
+                                                   i => i.AdjacencyRules);
+            Assert.AreEqual(150, result.GetGold(diamond)); // count=1 => +50%, не +100%
+        }
+
+        [Test]
+        public void Resolve_TwoBlocks_AdditiveSum_NotCompound()
+        {
+            var grid = new GridModel<Item>(3, 3);
+            var sword = Mk("sword", "X", 10, 1);
+            sword.AdjacencyRules = AdjacencyRule.ParseRules("hat|gold:+50%;sheath|gold:+50%");
+            grid.TryPlace(sword, 1, 1, Rotation.Deg0);
+            grid.TryPlace(Mk("hat", "X", 0, 1), 0, 1, Rotation.Deg0);
+            grid.TryPlace(Mk("sheath", "X", 0, 1), 2, 1, Rotation.Deg0);
+            var result = AdjacencyResolver.Resolve(grid, i => i.Id, i => i.Gold, i => i.AcidCost,
+                                                   i => i.AdjacencyRules);
+            Assert.AreEqual(20, result.GetGold(sword)); // +100% аддитивно (а не 10*1.5*1.5=22)
+        }
+
+        [Test]
+        public void Resolve_MultiTarget_SingleBlock_AppliesOnce()
+        {
+            var grid = new GridModel<Item>(3, 3);
+            var sword = Mk("sword", "X", 10, 1);
+            sword.AdjacencyRules = AdjacencyRule.ParseRules("hat,sheath|gold:+50%");
+            grid.TryPlace(sword, 1, 1, Rotation.Deg0);
+            grid.TryPlace(Mk("hat", "X", 0, 1), 0, 1, Rotation.Deg0);
+            grid.TryPlace(Mk("sheath", "X", 0, 1), 2, 1, Rotation.Deg0);
+            var result = AdjacencyResolver.Resolve(grid, i => i.Id, i => i.Gold, i => i.AcidCost,
+                                                   i => i.AdjacencyRules);
+            // нестак: блок срабатывает один раз несмотря на двух подходящих соседей
+            Assert.AreEqual(15, result.GetGold(sword));
+        }
+
+        [Test]
+        public void Resolve_Wildcard_HitsOnlyUnnamedNeighbors()
+        {
+            var grid = new GridModel<Item>(3, 3);
+            var coin = Mk("coin", "X", 100, 1);
+            coin.AdjacencyRules = AdjacencyRule.ParseRules("frog|gold:-50%;*|gold:+10%");
+            grid.TryPlace(coin, 1, 1, Rotation.Deg0);
+            grid.TryPlace(Mk("frog", "X", 0, 1), 0, 1, Rotation.Deg0);  // named => -50%
+            grid.TryPlace(Mk("bread", "X", 0, 1), 2, 1, Rotation.Deg0); // прочий => +10%
+            grid.TryPlace(Mk("gem", "X", 0, 1), 1, 0, Rotation.Deg0);   // прочий => +10%
+            var result = AdjacencyResolver.Resolve(grid, i => i.Id, i => i.Gold, i => i.AcidCost,
+                                                   i => i.AdjacencyRules);
+            // -50% + (+10%*2, нестак) = -40% (вайлдкард НЕ бьёт по frog)
+            Assert.AreEqual(60, result.GetGold(coin));
+        }
     }
 }
