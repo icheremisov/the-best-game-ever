d3419ecc05bc501307174349bb6853a02baa40c7
Ivan Cheremisov <devalidator@gmail.com>
2026-05-31 15:25:43 +0400

    config(adjacency): удалить столбец adjacencyTarget, склеить в adjacencyEffect
    Также обновлены EditMode-тесты (Code.Tests) под новый API
    AdjacencyRule.ParseRules / AdjacencyEffect.Parse — иначе проект не компилируется.

--- Game/Assets/Code.Tests/AdjacencyEffectTests.cs
+++ Game/Assets/Code.Tests/AdjacencyEffectTests.cs
@@ -6,44 +6,58 @@ namespace Mimic.Tests
     public class AdjacencyEffectTests
     {
         [Test]
-        public void ParseList_Empty_ReturnsEmpty()
+        public void ParseRules_Empty_ReturnsEmpty()
         {
-            Assert.AreEqual(0, AdjacencyEffect.ParseList("").Length);
-            Assert.AreEqual(0, AdjacencyEffect.ParseList(null).Length);
+            Assert.AreEqual(0, AdjacencyRule.ParseRules("").Length);
+            Assert.AreEqual(0, AdjacencyRule.ParseRules(null).Length);
         }
 
         [Test]
-        public void ParseList_SingleGoldPlus50()
+        public void Parse_SingleGoldPlus50()
         {
-            var fx = AdjacencyEffect.ParseList("gold:+50%");
-            Assert.AreEqual(1, fx.Length);
-            Assert.AreEqual(EffectType.Gold, fx[0].Type);
-            Assert.AreEqual(0.5f, fx[0].Multiplier, 0.0001f);
+            var fx = AdjacencyEffect.Parse("gold:+50%");
+            Assert.AreEqual(EffectType.Gold, fx.Type);
+            Assert.AreEqual(0.5f, fx.Multiplier, 0.0001f);
         }
 
         [Test]
-        public void ParseList_AcidMinus30()
+        public void Parse_AcidMinus30()
         {
-            var fx = AdjacencyEffect.ParseList("acid:-30%");
-            Assert.AreEqual(1, fx.Length);
-            Assert.AreEqual(EffectType.Acid, fx[0].Type);
-            Assert.AreEqual(-0.3f, fx[0].Multiplier, 0.0001f);
+            var fx = AdjacencyEffect.Parse("acid:-30%");
+            Assert.AreEqual(EffectType.Acid, fx.Type);
+            Assert.AreEqual(-0.3f, fx.Multiplier, 0.0001f);
         }
 
         [Test]
-        public void ParseList_Multiple_SemicolonSeparated()
+        public void Parse_OptionalSign()
         {
-            var fx = AdjacencyEffect.ParseList("gold:+50%;acid:-30%");
-            Assert.AreEqual(2, fx.Length);
-            Assert.AreEqual(EffectType.Gold, fx[0].Type);
-            Assert.AreEqual(EffectType.Acid, fx[1].Type);
+            Assert.AreEqual(0.05f, AdjacencyEffect.Parse("gold:5%").Multiplier, 0.0001f);
+        }
+
+        [Test]
+        public void Parse_StackableSuffix()
+        {
+            Assert.IsTrue(AdjacencyEffect.Parse("gold:+50%*").Stackable);
+            Assert.IsFalse(AdjacencyEffect.Parse("gold:+50%").Stackable);
+        }
+
+        [Test]
+        public void ParseRules_BlockWithTwoEffects()
+        {
+            var rules = AdjacencyRule.ParseRules("sword|gold:+50%,acid:-30%");
+            Assert.AreEqual(1, rules.Length);
+            Assert.AreEqual("sword", rules[0].Targets[0]);
+            Assert.AreEqual(2, rules[0].Effects.Length);
+            Assert.AreEqual(EffectType.Gold, rules[0].Effects[0].Type);
+            Assert.AreEqual(EffectType.Acid, rules[0].Effects[1].Type);
         }
 
         [Test]
         public void Parse_BadFormat_Throws()
         {
-            Assert.Throws<System.FormatException>(() => AdjacencyEffect.ParseList("gold+50"));
-            Assert.Throws<System.FormatException>(() => AdjacencyEffect.ParseList("foo:+50%"));
+            Assert.Throws<System.FormatException>(() => AdjacencyEffect.Parse("gold+50"));
+            Assert.Throws<System.FormatException>(() => AdjacencyEffect.Parse("foo:+50%"));
+            Assert.Throws<System.FormatException>(() => AdjacencyRule.ParseRules("sword"));
         }
     }
 }
