9d6c0cf07c73ced1d013be424b29e9676d26fe3a
Ivan Cheremisov <devalidator@gmail.com>
2026-05-31 22:56:23 +0400

    feat(combat): боевые поля в моделях данных

--- Game/Assets/Code/Data/AdventurerData.cs
+++ Game/Assets/Code/Data/AdventurerData.cs
@@ -6,5 +6,10 @@ namespace Mimic.Data
         public string Name;
         public string Phrase;
         public string[] LootIds;
+
+        // --- бой ---
+        public bool Battle;   // true => перед получением лута нужно сразиться
+        public int Hp;        // здоровье в бою
+        public int Attack;    // сила атаки в бою
     }
 }
