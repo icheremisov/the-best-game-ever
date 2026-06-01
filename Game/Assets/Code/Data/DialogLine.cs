01cabfa18575f8409531de5fc6c53f84a5e1aa2f
Ivan Cheremisov <devalidator@gmail.com>
2026-06-01 20:31:20 +0400

    feat(dialog): тип DialogLine

---
+++ Game/Assets/Code/Data/DialogLine.cs
@@ -0,0 +1,9 @@
+namespace Mimic.Data
+{
+    // Одна реплика диалога: текст и id портрета говорящего (master/mimic/id приключенца).
+    public class DialogLine
+    {
+        public string Text;
+        public string Icon;
+    }
+}
